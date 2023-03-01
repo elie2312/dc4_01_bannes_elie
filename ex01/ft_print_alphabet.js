19755b19d3d1e49963bd40c530e4b3b8910c2879
elie2312 <elie.bannes12@gmail.com>
2023-03-01 20:10:14 +0100

    ex01
    ex01 v1

---
+++ ex01/ft_print_alphabet.js
@@ -0,0 +1,9 @@
+let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+function printAlphabet(alphabetString) {
+  let alphabettableau = alphabet.split('');
+
+  console.table(alphabettableau);
+}
+
+printAlphabet(alphabet);
